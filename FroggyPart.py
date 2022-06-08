029c6980a06f0d05ff46e4a9875601bb6e9116ef
KeaneWong <Keanewong913@gmail.com>
2022-06-08 15:21:46 -0700

    added frogs

---
+++ FroggyPart.py
@@ -0,0 +1,135 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+LCD_RS = 26
+LCD_E  = 19
+LCD_D4 = 13 
+LCD_D5 = 6
+LCD_D6 = 5
+LCD_D7 = 11
+LED_ON = 15
+
+# Define some device constants
+LCD_WIDTH = 16    # Maximum characters per line
+LCD_CHR = True
+LCD_CMD = False
+
+LCD_LINE_1 = 0x80 # LCD RAM address for the 1st line
+LCD_LINE_2 = 0xC0 # LCD RAM address for the 2nd line 
+
+# Timing constants
+E_PULSE = 0.00005
+E_DELAY = 0.00005
+pirledpin = 18
+pirpin = 17
+globaltimer = 0.0
+
+
+#GPIO.add_event_detect(pirpin, GPIO.RISING)
+
+def pirloop():
+        lcd_init()
+        while True:
+            if GPIO.input(pirpin) == GPIO.HIGH:
+                GPIO.output(pirledpin, GPIO.HIGH)
+                lcd_byte(LCD_LINE_1, LCD_CMD)
+                lcd_string("ON",2)
+                print("on")
+
+            else:
+                GPIO.output(pirledpin, GPIO.LOW)
+                lcd_byte(LCD_LINE_1, LCD_CMD)
+                lcd_string("OFF",2)
+                print("off")
+            
+            
+def lcd_init():
+  GPIO.setmode(GPIO.BCM)       # Use BCM GPIO numbers
+  GPIO.setup(LCD_E, GPIO.OUT)  # E
+  GPIO.setup(LCD_RS, GPIO.OUT) # RS
+  GPIO.setup(LCD_D4, GPIO.OUT) # DB4
+  GPIO.setup(LCD_D5, GPIO.OUT) # DB5
+  GPIO.setup(LCD_D6, GPIO.OUT) # DB6
+  GPIO.setup(LCD_D7, GPIO.OUT) # DB7
+  GPIO.setup(LED_ON, GPIO.OUT) # Backlight enable  
+  # Initialise display
+  lcd_byte(0x33,LCD_CMD)
+  lcd_byte(0x32,LCD_CMD)
+  lcd_byte(0x28,LCD_CMD)
+  lcd_byte(0x0C,LCD_CMD)  
+  lcd_byte(0x06,LCD_CMD)
+  lcd_byte(0x01,LCD_CMD)  
+
+def lcd_string(message,style):
+
+  if style==1:
+    message = message.ljust(LCD_WIDTH," ")  
+  elif style==2:
+    message = message.center(LCD_WIDTH," ")
+  elif style==3:
+    message = message.rjust(LCD_WIDTH," ")
+
+  for i in range(LCD_WIDTH):
+    lcd_byte(ord(message[i]),LCD_CHR)
+
+def lcd_byte(bits, mode):
+  # Send byte to data pins
+  # bits = data
+  # mode = True  for character
+  #        False for command
+
+  GPIO.output(LCD_RS, mode) # RS
+
+  # High bits
+  GPIO.output(LCD_D4, False)
+  GPIO.output(LCD_D5, False)
+  GPIO.output(LCD_D6, False)
+  GPIO.output(LCD_D7, False)
+  if bits&0x10==0x10:
+    GPIO.output(LCD_D4, True)
+  if bits&0x20==0x20:
+    GPIO.output(LCD_D5, True)
+  if bits&0x40==0x40:
+    GPIO.output(LCD_D6, True)
+  if bits&0x80==0x80:
+    GPIO.output(LCD_D7, True)
+
+  # Toggle 'Enable' pin
+  time.sleep(E_DELAY)    
+  GPIO.output(LCD_E, True)  
+  time.sleep(E_PULSE)
+  GPIO.output(LCD_E, False)  
+  time.sleep(E_DELAY)      
+
+  # Low bits
+  GPIO.output(LCD_D4, False)
+  GPIO.output(LCD_D5, False)
+  GPIO.output(LCD_D6, False)
+  GPIO.output(LCD_D7, False)
+  if bits&0x01==0x01:
+    GPIO.output(LCD_D4, True)
+  if bits&0x02==0x02:
+    GPIO.output(LCD_D5, True)
+  if bits&0x04==0x04:
+    GPIO.output(LCD_D6, True)
+  if bits&0x08==0x08:
+    GPIO.output(LCD_D7, True)
+
+  # Toggle 'Enable' pin
+  time.sleep(E_DELAY)    
+  GPIO.output(LCD_E, True)  
+  time.sleep(E_PULSE)
+  GPIO.output(LCD_E, False)  
+  time.sleep(E_DELAY)               
+
+                
+if __name__ == '__main__':
+
+        GPIO.setup(pirledpin, GPIO.OUT)
+        GPIO.setup(pirpin, GPIO.IN)
+
+        GPIO.output(pirledpin, False)
+        pirloop()
+ 
